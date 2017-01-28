d891afbdb55c434b4cdf89ad0d59cd9d776203d5
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-01-28 17:34:55 +0100

    Migration scripts.

--- truffle/migrations/4_ratings_contract.js
+++ truffle/migrations/4_ratings_contract.js
@@ -5,12 +5,5 @@ module.exports = function(deployer, network) {
         "default": "0x995bef79dfa2e666de2c6e5f751b4483b6d05cd8",
         "test": "0x995bef79dfa2e666de2c6e5f751b4483b6d05cd8"
     };
-    if (network == "norsborg") {
-        // Deployed already
-        // deployer.deploy(BlockOneOracleClientTest, entitlementAddresses[network]);
-    } else if (network == "test") {
-        deployer.deploy(BlockOneOracleClientTest, entitlementAddresses[network]);
-    } else {
-        // Not needed otherwise
-    }
+    deployer.deploy(Ratings, entitlementAddresses[network]);
 };
