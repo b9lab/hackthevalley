7bfdbcb582cd60518b03c94ea6d7d6de0c1fa320
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-01-29 12:22:38 +0100

    Hacked modal to work with BlockOne.

--- www/app/js/modals.js
+++ www/app/js/modals.js
@@ -2,37 +2,50 @@ $(document).ready(function() {
     $("#submit-request").click(function() {
     	console.log("Request filed")
 
-        nameInput = $("#nameInput").val(),
-        urlInput = $("#urlInput").val(),
-        ricInput = $("#ricInput").val(),
-        hashInput = $("#hashInput").val(),
-        descriptionInput = $("#descriptionInput").val(),
-        amount = $("#etherInput").val()
+        nameInput = $("#nameInput").val();
+        urlInput = $("#urlInput").val();
+        ricInput = $("#ricInput").val();
+        hashInput = $("#hashInput").val();
+        descriptionInput = $("#descriptionInput").val();
+        amount = $("#etherInput").val();
+        maxAuditors = $("#maxAuditors").val();
 
         // TODO fetch deadline
-        // TODO fetch maxAuditors
-        // TODO feetch ipfsHash
         var deadline = new Date().getTime() + 86400;
+        // TODO remove overwrite maxAuditors
         var maxAuditors = 3;
-        var ipfsHash = "Qmwhateves";
-        return Ratings.deployed().submitRequestForRating.call(
-            nameInput, descriptionInput, ricInput,
-            urlInput, deadline, maxAuditors, ipfsHash,
-            { from: account, value: web3.toWei(amount) })
-            .then(function (success) {
-                if (!success) {
-                    console.log("Cannot submitRequestForRating. nameInput:", nameInput,
-                        ", descriptionInput", descriptionInput, ", ricInput", ricInput,
-                        ", urlInput", urlInput, ", deadline", deadline,
-                        ", maxAuditors", maxAuditors, ", ipfsHash", ipfsHash,
-                        ", account", account, ", ether", amount);
-                    // TODO inform user that it failed
-                    throw "Cannot submitRequestForRating";
-                }
-                return Ratings.deployed().submitRequestForRating.sendTransaction(
+        G_walletBar.createSecureSigner();
+        console.log(G_account);
+        console.log(Ratings.deployed())
+        var data = Ratings.deployed().contract.submitRequestForRating.getData(
                     nameInput, descriptionInput, ricInput,
-                    urlInput, deadline, maxAuditors, ipfsHash,
-                    { from: account, value: web3.toWei(amount) });
+                    urlInput, deadline, maxAuditors, hashInput);
+        console.log(data);
+        // return Ratings.deployed().submitRequestForRating.call(
+        //     nameInput, descriptionInput, ricInput,
+        //     urlInput, deadline, maxAuditors, hashInput,
+        //     { from: G_account, value: web3.toWei(amount) })
+        //     .then(function (success) {
+        //         if (!success) {
+        //             console.log("Cannot submitRequestForRating. nameInput:", nameInput,
+        //                 ", descriptionInput", descriptionInput, ", ricInput", ricInput,
+        //                 ", urlInput", urlInput, ", deadline", deadline,
+        //                 ", maxAuditors", maxAuditors, ", ipfsHash", hashInput,
+        //                 ", account", G_account, ", ether", amount);
+        //             // TODO inform user that it failed
+        //             throw "Cannot submitRequestForRating";
+        //         }
+                // return Ratings.deployed().submitRequestForRating.sendTransaction(
+                //     nameInput, descriptionInput, ricInput,
+                //     urlInput, deadline, maxAuditors, hashInput,
+                //     { from: G_account, value: web3.toWei(amount) })
+            // })
+        return web3.eth.sendTransactionPromise({
+                from: G_account,
+                to: Ratings.deployed().address,
+                value: web3.toWei(amount),
+                data: data,
+                gas: 3000000
             })
             .then(function (txHash) {
             	$(".modal").modal("hide");
