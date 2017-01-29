d8139a08665f4b72de10d07d90b3a7e1161278ab
Ibo Sy <ibo@b9lab.com>
2017-01-29 07:22:18 +0100

    add modal ajax loading. switch to debug jquery (NOT SLIM)

--- www/app/js/list.js
+++ www/app/js/list.js
@@ -1,20 +1,31 @@
 addListItem = function(logItemArgs) {
-	$("#list").append("<tr><td><a href='"+logItemArgs["uri"]+"'>"+logItemArgs["name"]+"</a></td><td>"+logItemArgs["ric"]+"</td><td>"+logItemArgs["reward"]+"</td><td class='text-center'><i class='fa fa-check fa-2x' aria-hidden='true'></i></td><td class='text-center'><button class='btn btn-success' data-toggle='modal' data-target='#issuerDetailModal'>DETAILS</button></td></tr>");
+    $("#list").append("<tr><td><a href='"+logItemArgs["uri"]+"'>"+logItemArgs["name"]+"</a></td><td>"+logItemArgs["ric"]+"</td><td>"+logItemArgs["reward"]+"</td><td class='text-center'><i class='fa fa-check fa-2x' aria-hidden='true'></i></td><td class='text-center'><button class='btn btn-success' data-toggle='modal' data-target='#detailsModal'>DETAILS</button></td></tr>");
 }
 
 buildList = function() {
-	Ratings.deployed().LogRequestForRatingSubmitted({}, {fromBlock: 0}).get(function(error, logs) {
-    	console.log(logs);
-    	for(var i=0; i<=logs.length; i++) {
-    		addListItem(logs[i].args);
-    	}
+    Ratings.deployed().LogRequestForRatingSubmitted({}, {fromBlock: 0}).get(function(error, logs) {
+        console.log(logs);
+        for(var i=0; i<=logs.length; i++) {
+            addListItem(logs[i].args);
+        }
     })
 }
 
 $(document).on("networkSet", function() {
-	buildList();
+    buildList();
 
-	Ratings.deployed().LogRequestForRatingSubmitted({}, {fromBlock: 'latest'}).watch(function(error, log) {
-    	addListItem(log.args);
+    Ratings.deployed().LogRequestForRatingSubmitted({}, {fromBlock: 'latest'}).watch(function(error, log) {
+        addListItem(log.args);
     })
+
+    setModalHandler();
 });
+
+setModalHandler = function(itemId) {
+    console.log("show");
+  $("#detailsModal").on("show.bs.modal", function(e) {
+    var link = $(e.relatedTarget);
+    var $modalBody = $("#detailsModal").find(".modal-body");
+    $modalBody.load("templates/list-details.html");
+  });
+}
