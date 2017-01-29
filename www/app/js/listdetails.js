3757e8a3efa02105b6504eabb9a21d1320f944d8
Ibo Sy <ibo@b9lab.com>
2017-01-29 09:23:44 +0100

    add listdetails files

---
+++ www/app/js/listdetails.js
@@ -0,0 +1,8 @@
+setListDetails = function(modal, listitem) {
+	// get list data
+    console.log("set details for ");
+    console.log(listitem);
+    console.log("modal:");
+    console.log(modal);
+    $(modal).find("#F_company_name").html("F_company_name");
+}
