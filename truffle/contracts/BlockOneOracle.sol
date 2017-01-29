29195b5fa116553dea351b3effea80f54fd19856
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-01-29 09:25:29 +0100

    Cleanup.

--- truffle/contracts/BlockOneOracle.sol
+++ truffle/contracts/BlockOneOracle.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.5;
 
 contract BlockOneOracle {
-    function request_IntraDay(bytes32 symbol,uint256 timestamp)returns (uint256 );
-    function request_EntityConnect(bytes32 uri1,bytes32 uri2,uint256 level)returns (uint256 );
-    function request_EndOfDay(bytes32 symbol,uint256 timestamp)returns (uint256 );
+    function request_IntraDay(bytes32 symbol, uint256 timestamp) returns (uint256 );
+    function request_EndOfDay(bytes32 symbol, uint256 timestamp) returns (uint256 );
+    function request_EntityConnect(bytes32 uri1, bytes32 uri2, uint256 level) returns (uint256 );
 }
 
